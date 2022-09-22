ac509d0a6f6e14a8c264ac63757e08bbe14aa7be
Bibi <bibi@gmail.com>
2022-09-22 17:40:31 -0300

    feito

---
+++ pw07.js
@@ -0,0 +1,10 @@
+
+    function media (){
+        //converte para float (parseFloat)
+            var a = parseFloat(n1.value);
+            var b = parseFloat(n2.value);
+            var nome = "Bibi";
+            var c = (a+b)/2;
+        //alert("Ola " + nome + " a media = " + c);
+            res.value = c;
+            }
